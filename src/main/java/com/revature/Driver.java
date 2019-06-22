b8d2d45fdfb8694acbd2f5e1d8a11336ccd3acf7
Aditya <adityajoshiusa@gmail.com>
2019-06-22 15:08:53 -0400

    Just have to add the custom exceptions

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import com.revature.model.Account;
 import com.revature.model.User;
+import com.revature.repository.AccountRepositoryJDBC;
 import com.revature.repository.UserRepositoryJDBC; 
 
 public class Driver {
@@ -12,6 +13,8 @@ public class Driver {
 		
 		Scanner input = new Scanner(System.in);
 		UserRepositoryJDBC user = new UserRepositoryJDBC();
+		AccountRepositoryJDBC account = new AccountRepositoryJDBC();
+		long accountNum = 0;
 		//Account account = new Account();
 		
 		while(true) {
@@ -29,6 +32,47 @@ public class Driver {
 
 				if(user.findIDandPassword(username, password)) {
 					System.out.println("Login Successful!");
+					
+					System.out.println("What would you like to do today?");
+					System.out.println("1. Deposit Money");
+					System.out.println("2. Withdraw Money");
+					System.out.println("3. View Balance");
+					
+					int choice = input.nextInt();
+					
+					switch(choice) {
+					case 1:
+//						System.out.println("Enter in account number:");
+//						accountNum = input.nextLong();
+						System.out.println("Enter in the amount you would like to deposit:");
+						double amount = input.nextDouble();
+						if(account.depositMoney(amount)) {
+							System.out.println("You have successfully deposited money!");
+						}
+						else {
+							System.out.println("Something is wrong!");
+						}
+						break;
+					case 2:
+//						System.out.println("Enter in account number:");
+//						accountNum = input.nextLong();
+						System.out.println("Enter in the amount you would like to withdraw:");
+						amount = input.nextDouble();
+						if(account.withdrawMoney(amount)) {
+							System.out.println("You have successfully withdrawn money!");
+						}
+						else {
+							System.out.println("Something is wrong!");
+						}
+						break;
+					case 3:
+						System.out.println("Enter in your account number:");
+						accountNum = input.nextLong();
+						System.out.println(account.lookAccount(accountNum));
+						break;
+					default:
+						break;
+					}
 
 				}
 				else {
