938a720b727950ee7d8b098fd410fe3653757cde
Aditya <adityajoshiusa@gmail.com>
2019-06-24 15:51:40 -0400

    Starting on the JUnit Tests

---
+++ src/test/java/banking/JunitTests.java
@@ -0,0 +1,30 @@
+package banking;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
+import com.revature.model.Account;
+import com.revature.model.User;
+import com.revature.repository.AccountRepositoryJDBC;
+import com.revature.repository.UserRepositoryJDBC;
+import com.revature.exception.*;
+
+public class JunitTests {
+	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
+	//@Test
+	
+	
+	//@Test
+	
+	
+
+}
